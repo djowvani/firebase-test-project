d19cb02af150a78da20d8b5913eb43a80da50e91
Giovani Anhesini Bezerra <gioanhesini@gmail.com>
2021-05-21 00:46:50 -0300

    Debugging for undefined "menoo"

--- codigo-fonte/MenooFans/functions/index.js
+++ codigo-fonte/MenooFans/functions/index.js
@@ -1,57 +1,66 @@
 const functions = require("firebase-functions");
 const firebase = require("firebase-admin");
 const express = require("express");
-var path = require('path');
-var exphbs = require('express-handlebars');
+var path = require("path");
+var exphbs = require("express-handlebars");
 
-const firebaseApp = firebase.initializeApp(
-    functions.config().firebase
-);
+const firebaseApp = firebase.initializeApp(functions.config().firebase);
 
 async function getMenooOfRestaurant(restName) {
-    return firebaseApp.firestore().collection("menu").where('shortName','==', restName).get();
+  return firebaseApp
+    .firestore()
+    .collection("menu")
+    .where("shortName", "==", restName)
+    .get();
 }
 
 var handlebars = exphbs.create({
-    layoutsDir: path.join(__dirname, "views/layouts"),
-    partialsDir: path.join(__dirname, "views/partials"),
-    defaultLayout: 'index',
-    extname: 'hbs',
-    helpers: require("./helpers/hbs.js").helpers
+  layoutsDir: path.join(__dirname, "views/layouts"),
+  partialsDir: path.join(__dirname, "views/partials"),
+  defaultLayout: "index",
+  extname: "hbs",
+  helpers: require("./helpers/hbs.js").helpers,
 });
 
-async function loadMenoo(restShortName){
-    var resMenoo = await getMenooOfRestaurant(restShortName);
-    var menuObj = undefined;
-    try{
-        //obtendo o restaurante ID.
-        resMenoo.forEach(docRest => {
-            menuObj = docRest.data();
-        });
-        return menuObj;
-    }catch (e){
-        console.error("BLA", e);
-    }
+async function loadMenoo(restShortName) {
+  console.log("restShortName: ", restShortName);
+  var resMenoo = await getMenooOfRestaurant(restShortName);
+  var menuObj = undefined;
+  try {
+    //obtendo o restaurante ID.
+    resMenoo.forEach((docRest) => {
+      menuObj = docRest.data();
+    });
+    return menuObj;
+  } catch (e) {
+    console.error("BLA", e);
+  }
 }
 
 const app = express();
-app.engine('hbs', handlebars.engine);
-app.set('view engine', 'hbs');
-app.set('views', './views');
+app.engine("hbs", handlebars.engine);
+app.set("view engine", "hbs");
+app.set("views", "./views");
 
-app.get('/profile/:name', (request, response) => {
-    response.set('Cache-Control','public, max-age=600, s-maxage=600');
-    const rest = request.params.name;
-    loadMenoo(rest).then(menoo => {
-        if(menoo === undefined) {
-            console.log("entrou 404");
-            response.status(404).render('404', { layout: '404' });
-        }else{
-            console.log("entrou no menoo");
-            response.render('index', {menoo});
-        }
-    }).catch(err=>{
-        console.log("Erro de execução na função", err);
+app.get("/profile/:name", (request, response) => {
+  //   console.log("começo de tudo: ", request);
+  //   console.log("começo de tudo 2: ", response);
+  response.set("Cache-Control", "public, max-age=600, s-maxage=600");
+  const rest = request.params.name;
+  console.log("rest: ", rest);
+  loadMenoo(rest)
+    .then((menoo) => {
+      console.log("oh o menoo: ", menoo);
+      if (menoo === undefined) {
+        console.log("entrou 404");
+        response.status(404).render("404", { layout: "404" });
+      } else {
+        console.log("entrou no menoo");
+        response.render("index", { menoo });
+      }
+    })
+    .catch((err) => {
+      console.log("Erro de execução na função", err);
     });
 });
 
